e278693fe6bd870dbc56eb96300390a921ef9165
Yuto299 <ota.se.ke@outlook.jp>
2025-02-13 10:12:36 +0900

    tailwind まだ

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+    },
+  },
+  plugins: [],
+};
